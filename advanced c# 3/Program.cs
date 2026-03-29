6022243ae2d14a2452079a069d40868add5bb525
Zeyad Hussein <zeyad02_hussein@hotmail.com>
2026-03-29 22:39:12 +0200

    Add project files.

---
+++ advanced c# 3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace advanced_c__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
